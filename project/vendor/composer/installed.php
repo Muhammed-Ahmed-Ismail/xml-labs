0755cd4d5dfe8c6cfee12c3070fbaa9fe2c30aef
Muhammed-Ismail <muhammadahmed.free@gmail.com>
2022-03-21 10:13:27 +0200

    xml-project-full-files

---
+++ project/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '1c5457011a85b9dee96ea4fa1d9e507314c0433d',
+        'name' => 'vendor_name/project',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor_name/project' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '1c5457011a85b9dee96ea4fa1d9e507314c0433d',
+            'dev_requirement' => false,
+        ),
+    ),
+);
